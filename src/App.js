4d4665dbbb5d9dae807b5e907fd6ce3b24424b6c
kierab <78439567+kierabon@users.noreply.github.com>
2021-06-24 15:36:22 -0500

    redirect page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navigation, Footer, Home, Projects, Contact } from "./components";
